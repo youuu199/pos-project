b0d283bc813f90700a0617c0b01f6c0f696d9fc7
Zar Ni Thhway <zarnithway.dev@gmail.com>
2026-03-16 10:31:17 +0630

    Clean repository

--- resources/views/admin/dashboard/home.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('admin.layouts.master');
-
-@section('content')
-    <h1>Welcome to Admin Dashboard</h1>
-@endsection
--- resources/views/user/dashboard/home.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('user.layouts.master');
-
-@section('content')
-    <h1>Welcome to User Dashboard</h1>
-@endsection
